765814a1f820d22112ae0b02df3a7a93c6291dbc
Nikolai <suvorovite699@mail.ru>
2020-04-10 21:29:00 +0300

    по замечаниям

--- reactjs/reactjs-5/src/index.js
+++ reactjs/reactjs-5/src/index.js
@@ -1,8 +1,7 @@
 async function promiseReduce(asyncFunctions, reduce, initialValue) {
     let v = initialValue
     for(let item of asyncFunctions) {
-        const p = await item();
-        v = reduce(p, v)
+        v = reduce(await item(), v);
     }
     return v
 }
