d52a2a5650d658de41c217bd35e930ca32ad2977
Nikolai <suvorovite699@mail.ru>
2020-04-10 20:55:04 +0300

    правки по замечаниям

--- reactjs/reactjs-5/src/index.js
+++ reactjs/reactjs-5/src/index.js
@@ -1,9 +1,9 @@
 async function promiseReduce(asyncFunctions, reduce, initialValue) {
-        let v = initialValue
-        for(let item of asyncFunctions) {
-            const p = await item();
-            v = reduce(p, v)
-        }
-        return new Promise(resolve => resolve(v))
+    let v = initialValue
+    for(let item of asyncFunctions) {
+        const p = await item();
+        v = reduce(p, v)
+    }
+    return v
 }
 module.exports = promiseReduce;
