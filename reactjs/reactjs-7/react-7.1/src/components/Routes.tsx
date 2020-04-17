d7dbceca1bf8437ad44c16335cd09df1175dbf82
Nikolai <suvorovite699@mail.ru>
2020-04-17 15:37:46 +0300

    7 Практическое задание. ReactJS. Файловая структура, состояния компонентов

---
+++ reactjs/reactjs-7/react-7.1/src/components/Routes.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import {Switch, Route, Redirect} from 'react-router-dom'
+import LoginForm from "../forms/LoginForm"
+import RegistrationForm from "../forms/RegistrationForm"
+
+export const useRoutes = (isAuthenticated: any) => {
+  if (isAuthenticated) {
+    return (
+      <Switch>
+        <Redirect to="/" />
+      </Switch>
+    )
+  }
+
+  return (
+    <Switch>
+      <Route path="/" exact>
+        <LoginForm />
+      </Route>
+      <Route path="/registration">
+        <RegistrationForm />
+      </Route>
+      <Redirect to="/" />
+    </Switch>
+  )
+}
